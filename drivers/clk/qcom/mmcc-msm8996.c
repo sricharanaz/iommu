3055ed97038899512d0f9e27be37ff360ec1bb14
Sricharan R <sricharan@codeaurora.org>
2016-07-04 12:31:30 +0530

    clk: qcom:mmcc: Model venus gdsc's using parent<->child relationship
    The current genpd framework assumes that there is only one Powerdomain
    associated with a device. As a result for device's having multiple
    power domains such as venus, the powerdomains have to represented
    in parent<->child relationship, to turnon all the required powerdomains.
    
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>

--- drivers/clk/qcom/mmcc-msm8996.c
+++ drivers/clk/qcom/mmcc-msm8996.c
@@ -2944,6 +2944,7 @@ static struct gdsc venus_core0_gdsc = {
 	.pd = {
 		.name = "venus_core0",
 	},
+	.parent = &venus_gdsc.pd,
 	.pwrsts = PWRSTS_OFF_ON,
 };
 
@@ -2954,6 +2955,7 @@ static struct gdsc venus_core1_gdsc = {
 	.pd = {
 		.name = "venus_core1",
 	},
+	.parent = &venus_core0_gdsc.pd,
 	.pwrsts = PWRSTS_OFF_ON,
 };
 
