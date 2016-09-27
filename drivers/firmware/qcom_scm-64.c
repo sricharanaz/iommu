968e388ec2a613b4ea572b30448e0d660d419e69
Sricharan R <sricharan@codeaurora.org>
2016-09-27 12:13:19 +0530

    firmware: qcom: scm: Fix interrupted SCM calls
    This patch fixes an issue with the SCM64 calls.  Sometimes SCM calls can
    be interrupted and return early.  When this happens, the contents of
    register 6 will contain a session ID that is required when resuming the
    SCM call.
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>

--- drivers/firmware/qcom_scm-64.c
+++ drivers/firmware/qcom_scm-64.c
@@ -131,12 +131,14 @@ static int qcom_scm_call(struct device *dev, u32 svc_id, u32 cmd_id,
 					 qcom_smccc_convention,
 					 ARM_SMCCC_OWNER_SIP, fn_id);
 
+		res->a6 = 0;
+
 		do {
 			arm_smccc_smc(cmd, desc->arginfo, desc->args[0],
-				      desc->args[1], desc->args[2], x5, 0, 0,
+				      desc->args[1], desc->args[2], x5, res->a6, 0,
 				      res);
 
-			printk(KERN_ALERT"\n arm_smccc_smc");
+			printk(KERN_ALERT"\n arm_smccc_smc res->a0 %d", res->a0);
 
 		} while (res->a0 == QCOM_SCM_INTERRUPTED);
 
