98bc1dce113e4731a8bb175ad6e871051eaa6f7e
Archit Taneja <architt@codeaurora.org>
2016-12-01 10:35:25 +0530

    WIP: driver core: Move pm get/put suppliers before/after driver probe
    An issue seen with device links fw. The fw doesn't calls get_suppliers
    before being aware of the links, but calls put_suppliers with the
    knowledge of the links, resulting in a mismatch of runtime get/put
    calls.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -394,6 +394,7 @@ re_probe:
 	 */
 	devices_kset_move_last(dev);
 
+	pm_runtime_get_suppliers(dev);
 	if (dev->bus->probe) {
 		ret = dev->bus->probe(dev);
 		if (ret)
@@ -403,6 +404,7 @@ re_probe:
 		if (ret)
 			goto probe_failed;
 	}
+	pm_runtime_put_suppliers(dev);
 
 	if (test_remove) {
 		test_remove = false;
@@ -435,6 +437,7 @@ re_probe:
 	goto done;
 
 probe_failed:
+	pm_runtime_put_suppliers(dev);
 	dma_deconfigure(dev);
 dma_failed:
 	if (dev->bus)
@@ -534,7 +537,6 @@ int driver_probe_device(struct device_driver *drv, struct device *dev)
 	pr_debug("bus: '%s': %s: matched device %s with driver %s\n",
 		 drv->bus->name, __func__, dev_name(dev), drv->name);
 
-	pm_runtime_get_suppliers(dev);
 	if (dev->parent)
 		pm_runtime_get_sync(dev->parent);
 
@@ -545,7 +547,6 @@ int driver_probe_device(struct device_driver *drv, struct device *dev)
 	if (dev->parent)
 		pm_runtime_put(dev->parent);
 
-	pm_runtime_put_suppliers(dev);
 	return ret;
 }
 
