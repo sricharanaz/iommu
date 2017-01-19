b31cb4d1eb10772da8c209881640d601bb90809f
Sricharan R <sricharan@codeaurora.org>
2017-01-19 19:47:33 +0530

    Revert "drivers: platform: Configure dma operations at probe time"
    This reverts commit d197cceae2ed0192870ef2826b7848c96a837054.
    
    Conflicts:
    	drivers/base/dd.c

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -19,7 +19,6 @@
 
 #include <linux/device.h>
 #include <linux/delay.h>
-#include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
@@ -356,10 +355,6 @@ re_probe:
 	if (ret)
 		goto pinctrl_bind_failed;
 
-	ret = dma_configure(dev);
-	if (ret)
-		goto dma_failed;
-
 	if (driver_sysfs_add(dev)) {
 		printk(KERN_ERR "%s: driver_sysfs_add(%s) failed\n",
 			__func__, dev_name(dev));
@@ -421,8 +416,6 @@ re_probe:
 	goto done;
 
 probe_failed:
-	dma_deconfigure(dev);
-dma_failed:
 	if (dev->bus)
 		blocking_notifier_call_chain(&dev->bus->p->bus_notifier,
 					     BUS_NOTIFY_DRIVER_NOT_BOUND, dev);
@@ -831,10 +824,6 @@ static void __device_release_driver(struct device *dev, struct device *parent)
 		else if (drv->remove)
 			drv->remove(dev);
 
-		dma_deconfigure(dev);
-
-		device_links_driver_cleanup(dev);
-		device_links_no_driver(dev);
 		devres_release_all(dev);
 		dev->driver = NULL;
 		dev_set_drvdata(dev, NULL);
