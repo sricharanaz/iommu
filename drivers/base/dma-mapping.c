56fb55d14ad93f098177975529a27107dde20fee
Sricharan R <sricharan@codeaurora.org>
2017-01-20 21:58:19 +0530

    Revert "Revert "With some more optimial""
    This reverts commit f783ff56436f172edd793a122ffe5b453b440a39.

--- drivers/base/dma-mapping.c
+++ drivers/base/dma-mapping.c
@@ -351,29 +351,26 @@ void dma_common_free_remap(void *cpu_addr, size_t size, unsigned long vm_flags)
 
 int dma_configure(struct device *dev)
 {
-	struct device *bridge = NULL, *dma_dev = dev;
-	enum dev_dma_attr attr;
-	int ret = 0;
+        struct device *acpi_dev = dev, *of_dev = dev, *bridge = NULL;
+        enum dev_dma_attr attr;
 
-	if (dev_is_pci(dev)) {
-		bridge = dma_dev = pci_get_host_bridge_device(to_pci_dev(dev));
-		if (IS_ENABLED(CONFIG_OF) && dma_dev->parent &&
-		    dma_dev->parent->of_node)
-			dma_dev = dma_dev->parent;
-	}
+        if (dev_is_pci(dev)) {
+                bridge = acpi_dev = pci_get_host_bridge_device(to_pci_dev(dev));
+                of_dev = bridge->parent;
+        }
 
-	if (dma_dev->of_node) {
-		ret = of_dma_configure(dev, dma_dev->of_node);
-	} else if (has_acpi_companion(dma_dev)) {
-		attr = acpi_get_dma_attr(to_acpi_device_node(dma_dev->fwnode));
-		if (attr != DEV_DMA_NOT_SUPPORTED)
-			ret = acpi_dma_configure(dev, attr);
-	}
+        if (IS_ENABLED(CONFIG_OF) && of_dev && of_dev->of_node) {
+                of_dma_configure(dev, of_dev->of_node);
+        } else if (has_acpi_companion(acpi_dev)) {
+                attr = acpi_get_dma_attr(to_acpi_device_node(acpi_dev->fwnode));
+                if (attr != DEV_DMA_NOT_SUPPORTED)
+                        acpi_dma_configure(dev, attr);
+        }
 
-	if (bridge)
-		pci_put_host_bridge_device(bridge);
+        if (bridge)
+                pci_put_host_bridge_device(bridge);
 
-	return ret;
+        return 0;
 }
 
 void dma_deconfigure(struct device *dev)
